92c6a19f741c5f51098ec8ee20196d6154828956
Jannik <jannik.developer02@gmail.com>
2025-02-22 13:48:52 +0100

    add plugin main class

---
+++ src/main/kotlin/club/mcsports/lobby/Lobby.kt
@@ -0,0 +1,15 @@
+package club.mcsports.lobby
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class Lobby : JavaPlugin() {
+
+    override fun onEnable() {
+        //Plugin startup logic
+    }
+
+    override fun onDisable() {
+        //Plugin shutdown logic
+    }
+
+}
