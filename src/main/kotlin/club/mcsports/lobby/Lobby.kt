eb52b67d0169c66abcb27c6d5dac47e295aa06ce
Jannik <jannik.developer02@gmail.com>
2025-02-23 23:30:50 +0100

    add interfaces lib

--- src/main/kotlin/club/mcsports/lobby/Lobby.kt
+++ src/main/kotlin/club/mcsports/lobby/Lobby.kt
@@ -1,11 +1,13 @@
 package club.mcsports.lobby
 
+import com.noxcrew.interfaces.InterfacesListeners
 import org.bukkit.plugin.java.JavaPlugin
 
 class Lobby : JavaPlugin() {
 
     override fun onEnable() {
         //Plugin startup logic
+        InterfacesListeners.install(this)
     }
 
     override fun onDisable() {
