968c0f20f5c13d85daf2b5ef93d0615fa27d067a
Jannik <jannik.developer02@gmail.com>
2025-02-27 23:23:13 +0100

    add text decoration

--- src/main/kotlin/club/mcsports/lobby/item/ItemComponents.kt
+++ src/main/kotlin/club/mcsports/lobby/item/ItemComponents.kt
@@ -1,4 +1,4 @@
-package club.mcsports.lobby.util
+package club.mcsports.lobby.item
 
 import club.mcsports.generated.PackBindings
 import club.mcsports.lobby.extension.miniMessage
@@ -9,14 +9,18 @@ import org.bukkit.Material
 import org.bukkit.NamespacedKey
 import org.bukkit.inventory.ItemStack
 
+/**
+ * General item components used in the lobby
+ */
 enum class ItemComponents(val component: Component, val lore: List<Component> = emptyList(), val model: PackModel? = null, val material: Material = Material.PAPER) {
 
-    GAME_SELECTOR(miniMessage("Game Selector"), material = Material.COMPASS),
-    GYM_BAG(miniMessage("Gym Bag"), material = Material.CAULDRON),
-    PROFILE(miniMessage("Profile"), material = Material.PLAYER_HEAD),
-    PARTY_INVITE(miniMessage("Invite Player"), model = PackBindings.RESET_ITEM.model),
-    PARTY_MEMBER(miniMessage("<player_name>"), material = Material.PLAYER_HEAD),
-    ARROW_LEFT(miniMessage("Previous"), material = Material.ARROW);
+    GAME_SELECTOR(miniMessage("<white>Game Selector"), material = Material.COMPASS),
+    GYM_BAG(miniMessage("<white>Gym Bag"), material = Material.CAULDRON),
+    PROFILE(miniMessage("<white>Profile"), material = Material.PLAYER_HEAD),
+    PARTY_INVITE(miniMessage("<white>Invite Player"), model = PackBindings.RESET_ITEM.model),
+    PARTY_MEMBER(miniMessage("<white><player_name>"), material = Material.PLAYER_HEAD),
+    CLOSE_MENU(miniMessage("<red>Close Menu"), material = Material.BARRIER),
+    ARROW_LEFT(miniMessage("<white>Previous"), material = Material.ARROW);
 
     fun build(): ItemStack {
         val itemStack = ItemStack(this.material)
