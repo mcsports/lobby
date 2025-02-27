a67e42994c563d214374ba365d9189d80729a600
Jannik <jannik.developer02@gmail.com>
2025-02-27 23:24:06 +0100

    add interaction basics

---
+++ src/main/kotlin/club/mcsports/lobby/listener/PlayerInteractListener.kt
@@ -0,0 +1,33 @@
+package club.mcsports.lobby.listener
+
+import club.mcsports.lobby.gui.GuiGameSelector
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import org.bukkit.NamespacedKey
+import org.bukkit.event.EventHandler
+import org.bukkit.event.Listener
+import org.bukkit.event.player.PlayerInteractEvent
+import org.bukkit.persistence.PersistentDataType
+
+class PlayerInteractListener : Listener {
+
+    private val gameSelector = GuiGameSelector()
+
+    @EventHandler
+    fun handlePlayerInteract(event: PlayerInteractEvent) {
+        val meta = event.item?.itemMeta ?: return
+
+        meta.persistentDataContainer.get(NamespacedKey("mcsports", "lobby/action"), PersistentDataType.STRING)?.let {
+            when(it) {
+                "open_game_selector" -> {
+                    CoroutineScope(Dispatchers.IO).launch {
+                        gameSelector.gui.open(event.player)
+                    }
+                }
+
+                else -> return
+            }
+        }
+    }
+}
