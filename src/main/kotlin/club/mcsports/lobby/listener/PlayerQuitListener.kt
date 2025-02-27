8d2b4f2888557cfa7a57c4a74448d28629a97f00
Jannik <jannik.developer02@gmail.com>
2025-02-27 15:58:42 +0100

    remove quit message

---
+++ src/main/kotlin/club/mcsports/lobby/listener/PlayerQuitListener.kt
@@ -0,0 +1,13 @@
+package club.mcsports.lobby.listener
+
+import org.bukkit.event.EventHandler
+import org.bukkit.event.Listener
+import org.bukkit.event.player.PlayerQuitEvent
+
+class PlayerQuitListener : Listener {
+
+    @EventHandler
+    fun onPlayerQuit(event: PlayerQuitEvent) {
+        event.quitMessage(null)
+    }
+}
