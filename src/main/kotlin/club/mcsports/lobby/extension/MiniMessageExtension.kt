d7bea2e2538e2dfecc8a09ae08355012e31cfb58
Jannik <jannik.developer02@gmail.com>
2025-02-27 23:23:36 +0100

    remove default italic decoration

--- src/main/kotlin/club/mcsports/lobby/extension/MiniMessageExtension.kt
+++ src/main/kotlin/club/mcsports/lobby/extension/MiniMessageExtension.kt
@@ -1,5 +1,6 @@
 package club.mcsports.lobby.extension
 
+import net.kyori.adventure.text.format.TextDecoration
 import net.kyori.adventure.text.minimessage.MiniMessage
 
-fun miniMessage(text: String) = MiniMessage.miniMessage().deserialize(text)
+fun miniMessage(text: String) = MiniMessage.miniMessage().deserialize(text).decoration(TextDecoration.ITALIC, false)
