ad1c02f95433d438e4159680537b926897937bf3
Jannik <jannik.developer02@gmail.com>
2025-02-28 17:34:41 +0100

    add indexed grid scissor function

--- src/main/kotlin/club/mcsports/lobby/extension/InterfaceExtension.kt
+++ src/main/kotlin/club/mcsports/lobby/extension/InterfaceExtension.kt
@@ -13,3 +13,21 @@ fun forEachInGridScissored(
         }
     }
 }
+
+
+fun forEachInGridScissoredIndexed(
+    startRow: Int,
+    endRow: Int,
+    startColumn: Int,
+    endColumn: Int,
+    function: (row: Int, column: Int, index: Int) -> Unit
+) {
+    var localIndex = 0
+
+    for (row in startRow..endRow) {
+        for (column in startColumn..endColumn) {
+            function(row, column, localIndex)
+            localIndex++
+        }
+    }
+}
