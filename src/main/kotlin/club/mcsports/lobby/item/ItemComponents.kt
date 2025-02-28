3dde7817319e25e722928fcfd13abaef95117679
Jannik <jannik.developer02@gmail.com>
2025-02-28 17:34:54 +0100

    separate lobby_server and lobby_server_unavailable

--- src/main/kotlin/club/mcsports/lobby/item/ItemComponents.kt
+++ src/main/kotlin/club/mcsports/lobby/item/ItemComponents.kt
@@ -2,9 +2,9 @@ package club.mcsports.lobby.item
 
 import club.mcsports.generated.PackBindings
 import club.mcsports.lobby.extension.miniMessage
+import club.mcsports.lobby.extension.toMiniFont
 import io.github.solid.resourcepack.api.link.ModelLink
 import net.kyori.adventure.text.Component
-import org.bukkit.Bukkit
 import org.bukkit.Material
 import org.bukkit.NamespacedKey
 import org.bukkit.inventory.ItemStack
@@ -16,7 +16,7 @@ enum class ItemComponents(
     val component: Component,
     val lore: List<Component> = emptyList(),
     val model: ModelLink? = null,
-    val material: Material = Material.PAPER
+    val material: Material = Material.PAPER,
 ) {
 
     GAME_SELECTOR(miniMessage("<white>Game Selector"), material = Material.COMPASS),
@@ -25,7 +25,22 @@ enum class ItemComponents(
     PARTY_INVITE(miniMessage("<white>Invite Player"), model = PackBindings.INVITE_TO_PARTY_ITEM.model),
     PARTY_MEMBER(miniMessage("<white><player_name>"), material = Material.PLAYER_HEAD),
     CLOSE_MENU(miniMessage("<red>Close Menu"), material = Material.BARRIER),
-    LOBBY_SERVER(miniMessage("<color:#58cbed>Lobby <service_number>"), material = Material.FLOW_BANNER_PATTERN),
+    LOBBY_SERVER(
+        miniMessage("<color:#58cbed>Lobby <service_number>"), lore = listOf(
+            miniMessage("<gray><italic>${"Click to connect".toMiniFont()}"),
+            Component.empty(),
+            miniMessage("<dark_gray><strikethrough>${"-".repeat(15)}"),
+            miniMessage("<white>Online: <color:#bee7fa><online_player_count>")
+        ), material = Material.FLOW_BANNER_PATTERN
+    ),
+    LOBBY_SERVER_UNAVAILABLE(
+        miniMessage("<color:#58cbed>${"Lobby <service_number>".toMiniFont()}"), lore = listOf(
+            miniMessage("<red>${"Currently on this server".toMiniFont()}"),
+            Component.empty(),
+            miniMessage("<dark_gray><strikethrough>${"-".repeat(15)}"),
+            miniMessage("<white>Online: <color:#bee7fa><online_player_count>")
+        ), material = Material.FLOW_BANNER_PATTERN
+    ),
     ARROW_LEFT(miniMessage("<white>Previous"), material = Material.ARROW);
 
     fun build(): ItemStack {
