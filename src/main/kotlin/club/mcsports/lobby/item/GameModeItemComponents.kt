4c06a847cd4fbcef9754086283b8a7853201a257
Jannik <jannik.developer02@gmail.com>
2025-03-04 17:13:05 +0100

    add new textures

--- src/main/kotlin/club/mcsports/lobby/item/GameModeItemComponents.kt
+++ src/main/kotlin/club/mcsports/lobby/item/GameModeItemComponents.kt
@@ -1,5 +1,6 @@
 package club.mcsports.lobby.item
 
+import club.mcsports.generated.PackBindings
 import club.mcsports.lobby.extension.miniMessage
 import club.mcsports.lobby.extension.toMiniFont
 import net.kyori.adventure.text.Component
@@ -20,7 +21,8 @@ enum class GameModeItemComponents(
                 Component.empty(),
                 miniMessage("<color:#bee7fa>Try your best to get the ball into"),
                 miniMessage("<color:#bee7fa>the hole with the fewest strokes"),
-            )
+            ),
+            model = PackBindings.GOLF_ICON_ITEM.model
         )
     ),
 
@@ -32,7 +34,8 @@ enum class GameModeItemComponents(
                 Component.empty(),
                 miniMessage("<color:#bee7fa>Fly through the map and score"),
                 miniMessage("<color:#bee7fa>the best time to win"),
-            )
+            ),
+            model = PackBindings.GLIDE_ICON_ITEM.model
         )
     ),
 
@@ -44,7 +47,8 @@ enum class GameModeItemComponents(
                 Component.empty(),
                 miniMessage("<color:#bee7fa>Knock down all the pins to win"),
                 miniMessage("<color:#bee7fa>and get the best score"),
-            )
+            ),
+            model = PackBindings.BOWLING_ICON_ITEM.model
         )
     ),
 
@@ -56,7 +60,8 @@ enum class GameModeItemComponents(
                 Component.empty(),
                 miniMessage("<color:#bee7fa>Hurry up and give the customers"),
                 miniMessage("<color:#bee7fa>their food in time to win"),
-            )
+            ),
+            model = PackBindings.MASTERCHEFS_ICON_ITEM.model
         )
     ),
 
@@ -68,7 +73,8 @@ enum class GameModeItemComponents(
                 Component.empty(),
                 miniMessage("<color:#bee7fa>Run through the map with insane speed"),
                 miniMessage("<color:#bee7fa>and be the first to finish."),
-            )
+            ),
+            model = PackBindings.SPRINT_ICON_ITEM.model
         )
     ),
 
@@ -104,7 +110,8 @@ enum class GameModeItemComponents(
                 Component.empty(),
                 miniMessage("<color:#bee7fa>Lorem ipsum dolor sit amet,"),
                 miniMessage("<color:#bee7fa>consectetur adipiscing elit."),
-            )
+            ),
+            model = PackBindings.POOL_ICON_ITEM.model
         )
     ),
 
