a7a41d75bb8380562d76ea1e0badb5ea8c71b2b0
Jannik <jannik.developer02@gmail.com>
2025-03-06 10:05:31 +0100

    add spawn points

---
+++ src/main/kotlin/club/mcsports/lobby/location/SpawnPoint.kt
@@ -0,0 +1,14 @@
+package club.mcsports.lobby.location
+
+enum class SpawnPoint {
+
+    SPAWN,
+    CLUBHOUSE,
+    BOWLING,
+    GLIDE,
+    SPRINT,
+    MASTER_CHEFS,
+    POWER_GOLF,
+    POOL
+
+}
