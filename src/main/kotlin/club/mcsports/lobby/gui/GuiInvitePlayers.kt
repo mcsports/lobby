0924f7e367304bd4d041933d02fb0b57c0842284
Jannik <jannik.developer02@gmail.com>
2025-04-08 13:59:18 +0200

    separate transform with triggers

--- src/main/kotlin/club/mcsports/lobby/gui/GuiInvitePlayers.kt
+++ src/main/kotlin/club/mcsports/lobby/gui/GuiInvitePlayers.kt
@@ -5,6 +5,8 @@ import club.mcsports.lobby.extension.miniMessage
 import club.mcsports.lobby.extension.toMiniFont
 import club.mcsports.lobby.item.ItemComponents
 import club.mcsports.lobby.util.Party
+import club.mcsports.lobby.util.PartyMember
+import club.mcsports.lobby.util.PartyRole
 import com.noxcrew.interfaces.InterfacesConstants
 import com.noxcrew.interfaces.drawable.Drawable.Companion.drawable
 import com.noxcrew.interfaces.element.StaticElement
@@ -32,7 +34,7 @@ class GuiInvitePlayers(party: Party) {
 
         rows = 5
 
-        withTransform(interfaceProperty(invites)) { pane, view ->
+        withTransform { pane, view ->
 
             pane[0, 8] = StaticElement(drawable(ItemComponents.CLOSE_MENU.build())) {
                 CoroutineScope(Dispatchers.IO).launch {
@@ -40,7 +42,6 @@ class GuiInvitePlayers(party: Party) {
                 }
             }
 
-            // Question mark
             pane[2, 1] = StaticElement(drawable(ItemComponents.QUESTION_MARK.build().also { item ->
                 item.editMeta { meta ->
                     meta.displayName(miniMessage("<white>Click to enter players"))
@@ -52,7 +53,7 @@ class GuiInvitePlayers(party: Party) {
                 view.runChatQuery { component ->
                     invites.addAll(
                         LegacyComponentSerializer.legacyAmpersand().serialize(component).split(",", " ")
-                            .filter { it.isNotBlank() && it.matches(nameRegex) && Bukkit.getPlayerExact(it) != null }
+                            .filter { it.isNotBlank() && it.matches(nameRegex) }
                             .toSet()
                     )
 
@@ -62,52 +63,22 @@ class GuiInvitePlayers(party: Party) {
                         return@runChatQuery false
                     }
 
-                    forEachInGridScissoredIndexed(5, 7, 0, 8) { x, y, index ->
-                        if (index >= invites.size) return@forEachInGridScissoredIndexed
-
-                        pane[x, y] = StaticElement(drawable(ItemComponents.PARTY_MEMBER.build().also { item ->
-                            item.editMeta { meta ->
-                                meta.displayName(meta.displayName()?.replaceText {
-                                    it.matchLiteral("<player_name>").replacement(invites.elementAt(index))
-                                })
-
-                                meta.lore(
-                                    listOf(
-                                        miniMessage("<gray>${"Click to remove".toMiniFont()}")
-                                    )
-                                )
-
-                                meta.persistentDataContainer.let { container ->
-                                    container.set(
-                                        NamespacedKey("mcsports", "lobby/player"),
-                                        PersistentDataType.STRING,
-                                        invites.elementAt(index)
-                                    )
-                                    container.set(
-                                        NamespacedKey("mcsports", "lobby/action"),
-                                        PersistentDataType.STRING,
-                                        "remove_member_from_invites"
-                                    )
-                                }
-
-                            }
-                        })) {
-                            invites.removeAt(index)
-                            view.redrawComplete()
-                        }
-                    }
-
                     true
                 }
             }
 
-            // Check mark
             pane[2, 7] = StaticElement(drawable(ItemComponents.CHECK_MARK.build().also { item ->
                 item.editMeta { meta ->
                     meta.displayName(miniMessage("<white>Invite Players"))
                     meta.lore(listOf(miniMessage("<gray>${"Click to confirm".toMiniFont()}")))
                 }
             })) { _ ->
+                if(invites.isEmpty()) {
+                    view.player.sendMessage(miniMessage("䶵 <color:#dc2626>Aborting</color><white> party invite! <gray>(no players to invite)"))
+                    InterfacesConstants.SCOPE.launch { view.close(InventoryCloseEvent.Reason.CANT_USE) }
+                    return@StaticElement
+                }
+
                 val offlinePlayers = mutableListOf<String>()
 
                 invites.forEach { inviteName ->
@@ -130,10 +101,49 @@ class GuiInvitePlayers(party: Party) {
                     )
                     else view.player.sendMessage(miniMessage("䶵 <white>All players have been invited!"))
 
-                    party.entries.addAll(invites.mapNotNull { Bukkit.getPlayer(it)?.uniqueId })
+                    party.entries.addAll(invites.map { PartyMember(it, PartyRole.MEMBER) })
 
                     invites.clear()
                     offlinePlayers.clear()
+                }
+            }
+
+        }
+
+        withTransform(interfaceProperty(invites)) { pane, view ->
+
+            forEachInGridScissoredIndexed(5, 7, 0, 8) { x, y, index ->
+                if (index >= invites.size) return@forEachInGridScissoredIndexed
+
+                pane[x, y] = StaticElement(drawable(ItemComponents.PARTY_MEMBER.build().also { item ->
+                    item.editMeta { meta ->
+                        meta.displayName(meta.displayName()?.replaceText {
+                            it.matchLiteral("<player_name>").replacement(invites[index])
+                        })
+
+                        meta.lore(
+                            listOf(
+                                miniMessage("<gray>${"Click to remove".toMiniFont()}")
+                            )
+                        )
+
+                        meta.persistentDataContainer.let { container ->
+                            container.set(
+                                NamespacedKey("mcsports", "lobby/player"),
+                                PersistentDataType.STRING,
+                                invites[index]
+                            )
+                            container.set(
+                                NamespacedKey("mcsports", "lobby/action"),
+                                PersistentDataType.STRING,
+                                "remove_member_from_invites"
+                            )
+                        }
+
+                    }
+                })) {
+                    invites.removeAt(index)
+                    view.redrawComplete()
                 }
             }
         }
