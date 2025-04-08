85ce306fe9d6660c72d6ed6a4c8ac16c786efd7d
Jannik <jannik.developer02@gmail.com>
2025-04-08 14:00:00 +0200

    add party member role

---
+++ src/main/kotlin/club/mcsports/lobby/util/PartyRole.kt
@@ -0,0 +1,20 @@
+package club.mcsports.lobby.util
+
+enum class PartyRole {
+
+    /**
+     * Does everything
+     */
+    OWNER,
+
+    /**
+     * Can kick and invite members
+     */
+    MODERATOR,
+
+    /**
+     * Just a member who can suggest invites
+     */
+    MEMBER
+
+}
